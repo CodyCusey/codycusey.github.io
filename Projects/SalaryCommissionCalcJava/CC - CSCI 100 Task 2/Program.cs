a819db928b9cd9f65a18a7e5a79750afacef765c
Cody Cusey <cody.cusey@mail.helenacollege.edu>
2025-04-23 23:16:26 -0600

    go

---
+++ Projects/SalaryCommissionCalcJava/CC - CSCI 100 Task 2/Program.cs
@@ -0,0 +1,74 @@
+﻿// Cody Cusey - 11/28/2022
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC___CSCI_100_Task_2
+{
+    internal class Program
+    {
+        public static Dictionary<int, SalesPerson> SalePersons = new Dictionary<int, SalesPerson>();
+
+        static void Main(string[] args)
+        {
+            int numOfPersons = 0;
+            string firstName;
+            int level = 0;
+            double hoursWorked = 0;
+            double amount = 0;
+            double totalComm = 0;
+            double totalPay = 0;
+            double totalBonus = 0;
+            double total = 0;
+
+            Console.Write("Enter Number of Sales Persons You Would Like to Calculate For: ");
+            numOfPersons = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n");
+
+            for(int x = 0; x < numOfPersons; x++)
+            {
+                Console.Write("Enter Sales Person First Name: ");
+                firstName = Console.ReadLine();
+                Console.Write("Enter Employee Sales Comission Level (1=Low / 4=High): ");
+                try
+                {
+                    level = Convert.ToInt32(Console.ReadLine());
+                }
+                catch 
+                {
+                    level = 0;
+                    Console.WriteLine("Invalid entry, please try again.");
+                }
+                Console.Write("Enter Weekly Hours Worked: ");
+                hoursWorked = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Enter Sales Amount (in USD): ");
+                amount = Convert.ToDouble(Console.ReadLine());
+                Console.Write("\n");
+
+                SalesPerson newPerson = new SalesPerson(x+1, firstName, level, amount, hoursWorked);
+                SalePersons.Add(newPerson.id, newPerson);
+
+                totalComm += newPerson.CalculateCommission();
+                totalPay += newPerson.CalculatePay();
+                totalBonus += newPerson.CalculateBonus();
+            }
+            total = totalComm + totalPay + totalBonus;
+            Console.WriteLine("Total Commission: {0:C}", totalComm);
+            Console.WriteLine("Total Wage Pay: {0:C}", totalPay);
+            Console.WriteLine("Total Bonus: {0:C}", totalBonus);
+            Console.WriteLine("Total Combined: {0:C}\n", total);
+            
+            
+            foreach(SalesPerson person in SalePersons.Values)
+            {
+                Console.WriteLine(person);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Projects/SalaryCommissionCalcJava/CC - CSCI 100 Task 2/SalesPerson.cs
@@ -0,0 +1,150 @@
+﻿// Cody Cusey - 11/28/2022
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC___CSCI_100_Task_2
+{
+    internal class SalesPerson
+    {
+        public string name { get; set; }
+        public int level { get; set; }
+        public double sales { get; set; }
+        public double hoursWorked { get; set; }
+        public int id { get; set; }
+
+        public SalesPerson(int id, string name, int level, double sales, double hoursWorked)
+        {
+            this.id = id;
+            this.name = name;
+            this.level = level;
+            this.sales = sales;
+            this.hoursWorked = hoursWorked;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Employee Name: {0} \nCommission: {1:C} \nWage Pay: {2:C} \nBonus: {3:C} \nGross Pay: {4:C}\n", name, CalculateCommission(), CalculatePay(), CalculateBonus(), CalculateGrandTotal());
+        }
+
+        public double CalculateCommission()
+        {
+            double totalComm = 0;
+            double commRate = 0;
+
+            if (level == 1)
+            {
+                if (sales <= 2000)
+                {
+                    commRate = .0525;
+                }
+                else
+                {
+                    if (sales < 3500)
+                    {
+                        commRate = .075;
+                    }
+                    else
+                    {
+                        commRate = .1075;
+                    }
+                }
+            }
+            else
+            {
+                if (level == 2)
+                {
+                    if (sales <= 2800)
+                    {
+                        commRate = .065;
+                    }
+                    else
+                    {
+                        commRate = .0775;
+                    }
+                }
+                else
+                {
+                    if (level == 3)
+                    {
+                        commRate = .0825;
+                    }
+                    else
+                    {
+                        if (level == 4)
+                        {
+                            commRate = .0975;
+                        }
+                        else
+                        {
+                            commRate = 0;
+                        }
+                    }
+                }
+            }
+            return totalComm = sales * commRate;
+
+        }
+
+        public double CalculatePay()
+        {
+            double hourlyRate = 0;
+            double totalPay = 0;
+            double regularPay = 0;
+            double otPay = 0;
+
+            switch (level)
+            {
+                case 1:
+                    hourlyRate = 12.75;
+                    break;
+                case 2:
+                    hourlyRate = 14.25;
+                    break;
+                case 3:
+                    hourlyRate = 17.50;
+                    break;
+                case 4:
+                    hourlyRate = 21.75;
+                    break;
+            }
+
+            regularPay = hourlyRate * hoursWorked;
+
+            if(hoursWorked > 40)
+            {
+                otPay = (hoursWorked - 40) * (hourlyRate * 1.5);
+                regularPay = hourlyRate * 40;
+            }
+
+            return totalPay = regularPay + otPay;
+        }
+
+        public double CalculateBonus()
+        {
+            double bonusTotal = 0;
+
+            if(sales > 24000 && level > 0 && level < 5)
+            {
+                bonusTotal = .03;
+                bonusTotal = sales * bonusTotal;
+            }
+
+            return bonusTotal;
+        }
+
+        public double CalculateGrandTotal()
+        {
+            double grandTotal = CalculateBonus() + CalculateCommission() + CalculatePay();
+
+            return grandTotal;
+        }
+
+
+
+
+    }
+}
