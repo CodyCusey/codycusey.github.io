711d65c6a672ce655113679e84896350171cf07c
Cody Cusey <cody.cusey@mail.helenacollege.edu>
2025-04-26 10:46:23 -0600

    try again

--- Projects/AlbumTrackerVue/src/views/addconfirm.vue
+++
@@ -1,7 +0,0 @@
-<!-- Just a super basic confirmation splash page, to inform the user of success -->
-
-<template>
-    <div class="confirmation">
-      <h1>Album successfully added!</h1>
-    </div>
-  </template>
